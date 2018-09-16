477e5ccc761c78ef8a2c7ac36fe242a70f02bf2c
Natalio Ruiz <natalio.ruiz@siriusxm.com>
2018-09-16 08:21:07 -0300

    jsonvalidator first commit

---
+++ activity/jsonvalidator/activity.go
@@ -0,0 +1,40 @@
+package uuid
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+var log = logger.GetLogger("activity-json-validator")
+
+type JSONValidatorActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new GPIOActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &JSONValidatorActivity{metadata: metadata}
+}
+
+// Metadata returns the activity's metadata
+func (a *JSONValidatorActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements api.Activity.Eval - Invokes a REST Operation
+func (a *JSONValidatorActivity) Eval(context activity.Context) (done bool, err error) {
+	jsonDoc, _ := context.GetInput("document").(string)
+	jsonSchema, _ := context.GetInput("schema").(string)
+
+	doc := gojsonschema.NewStringLoader(jsonDoc)
+	schema := gojsonschema.NewStringLoader(jsonSchema)
+
+	result, err := gojsonschema.Validate(schema, doc)
+	if err != nil {
+		return false, err
+	}
+
+	context.SetOutput("valid", result.Valid())
+	return true, nil
+}
---
+++ activity/jsonvalidator/activity_test.go
@@ -0,0 +1,68 @@
+package uuid
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestJsonSchema(t *testing.T) {
+	a := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	var jsonSchema = `
+	{
+		"$schema": "http://json-schema.org/draft-04/schema#",
+		"type": "object",
+		"properties": {
+			"data": {"type": "string"}
+		}
+	}
+	`
+
+	var jsonDoc = `
+	{
+		"data": "this is data"
+	}
+	`
+
+	tc.SetInput("document", jsonDoc)
+	tc.SetInput("schema", jsonSchema)
+
+	a.Eval(tc)
+	val := tc.GetOutput("valid")
+
+	if val.(bool) != true {
+		t.Errorf("JSON should be valid")
+	}
+
+}
